3f55e334c808f81c9385ff2e2f4f899fe234f5b4
bruce.wang <bruce.wang@zerologix.com>
2019-10-11 16:35:22 +1100

    clear branch

--- functions.js
+++ functions.js
@@ -1,25 +1,54 @@
-const origin = function laidDown(text){
-console.log(laidDown)
+// const origin = function laidDown(text){
+// console.log(laidDown)
 
+// }
+// //origin('holfman display')
+// //laidDown('second name ')
+
+// const fun1 = (params)=>{
+
+// 	console.log(fun1)
+// }
+
+// fun1('just here')
+
+// function ordinary(params){
+// ordinary(console.log(params))
+// }
+// //ordiary('you are inside ordinary function ') 
+// function rordinary (params){
+// return rordinary ('stops here')
+// }
+
+// //rordinary ('you got me there')
+
+// // this can be different here 
+
+
+// const fancy=function extract({pa1 ='jakes',pa2='shake'}){
+// console.log(pa1)
+// console.log(pa2)
+// }
+
+// //fancy() 
+// //extract()
+// function showLog(){
+// console.log('shows youre log here')
+// }
+// //fancy function2 here need to be tested
+// let pa3 = 'values outside here '
+// const fancy2 = showLog(()=>{
+// 	pa4 = pa3 +'values inside '
+// 	console.log( pa4)	
+// },{pa3})() 
+
+// fancy2()
+
+let title ='justings';
+let job = 'holfman';
+
+const object = ({title,job})=>{
+	 console.log(`you title is ${title} , your job is ${job}`)
 }
-//origin('holfman display')
-//laidDown('second name ')
 
-const fun1 = (params)=>{
-
-	console.log(fun1)
-}
-
-fun1('just here')
-
-function ordinary(params){
-ordinary(console.log(params))
-}
-//ordiary('you are inside ordinary function ') 
-function rordinary (params){
-return rordinary ('stops here')
-}
-
-rordinary ('you got me there')
-
-
+object({title,job})
